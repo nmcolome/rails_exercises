2a25224aa78e88bfc43bc23e01dc7f6048b93373
NataliaColome <nmcolome@gmail.com>
2019-11-07 01:05:19 -0700

    Refactor routes with 'resources' sintax

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,36 @@
 Rails.application.routes.draw do
 
-  get '/dogs', to: 'dogs#index'
-  get '/dogs/new', to: 'dogs#new'
-  get '/dogs/:id', to: 'dogs#show', as: 'dog'
-  get '/dogs/:id/edit', to: 'dogs#edit'
-  post '/dogs', to: 'dogs#create'
-  patch '/dogs/:id', to: 'dogs#update'
-  delete '/dogs/:id', to: 'dogs#destroy'
+  resources :dogs
+  # get '/dogs', to: 'dogs#index'
+  # get '/dogs/new', to: 'dogs#new'
+  # get '/dogs/:id', to: 'dogs#show', as: 'dog'
+  # get '/dogs/:id/edit', to: 'dogs#edit'
+  # post '/dogs', to: 'dogs#create'
+  # patch '/dogs/:id', to: 'dogs#update'
+  # delete '/dogs/:id', to: 'dogs#destroy'
 
-  get '/dogs/:dog_id/tricks', to: 'tricks#index', as: 'dog_tricks'
-  get '/dogs/:dog_id/tricks/new', to: 'tricks#new'
-  post '/dogs/:dog_id/tricks', to: 'tricks#create'
+  resources :dogs do
+    resources :tricks, only: [:index, :new, :create]
+  end
+  # get '/dogs/:dog_id/tricks', to: 'tricks#index', as: 'dog_tricks'
+  # get '/dogs/:dog_id/tricks/new', to: 'tricks#new'
+  # post '/dogs/:dog_id/tricks', to: 'tricks#create'
 
   get '/hello', to: 'nonrestful#hello'
   get '/greetings/:name', to: 'nonrestful#what_is_your_name'
   patch '/items/:the_item_id/deactivate', to: 'nonrestful#deactivate'
   delete '/subtract/:x/from/:y', to: 'nonrestful#subtract', as: 'subtract'
 
-  get '/admin/users/:id', to: 'admin/users#show', as: 'admin_user'
-  get '/admin/dogs', to: 'admin/dogs#index'
-  get '/admin/dogs/:dog_id/tricks', to: 'admin/tricks#index'
-  delete '/admin/tricks/:id', to: 'admin/tricks#destroy'
+  namespace :admin do
+    resources :users, only: [:show]
+    resources :tricks, only: [:destroy]
+    resources :dogs, only: [:index]
+    resources :dogs do
+      resources :tricks, only: [:index]
+    end
+  end
+  # get '/admin/users/:id', to: 'admin/users#show', as: 'admin_user'
+  # get '/admin/dogs', to: 'admin/dogs#index'
+  # get '/admin/dogs/:dog_id/tricks', to: 'admin/tricks#index'
+  # delete '/admin/tricks/:id', to: 'admin/tricks#destroy'
 end
