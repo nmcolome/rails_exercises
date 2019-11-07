df4736af03ad8228f5d8320a35cd83a3ee2b7dc4
NataliaColome <nmcolome@gmail.com>
2019-11-07 00:08:31 -0700

    Add Dog RESTful routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/dogs', to: 'dogs#index'
+  get '/dogs/new', to: 'dogs#new'
+  get '/dogs/:id', to: 'dogs#show'
+  get '/dogs/:id/edit', to: 'dogs#edit'
+  post '/dogs', to: 'dogs#create'
+  patch '/dogs/:id', to: 'dogs#update'
+  delete '/dogs/:id', to: 'dogs#destroy'
 end
