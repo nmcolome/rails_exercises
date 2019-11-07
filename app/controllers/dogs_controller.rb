ff5b71a4fbe7ef498a0d1e171da2423f5185a47d
NataliaColome <nmcolome@gmail.com>
2019-11-06 17:56:55 -0700

    Create DogController & add CRUD actions

---
+++ app/controllers/dogs_controller.rb
@@ -0,0 +1,35 @@
+class DogsController < ApplicationController
+  def index
+    @dogs = Dog.all
+  end
+
+  def show
+    @dog = Dog.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    Dog.create!(name: params[:name], breed: params[:breed], age: params[:age])
+
+    redirect_to :dogs
+  end
+
+  def edit
+    @dog = Dog.find(params[:id])
+  end
+
+  def update
+    @dog = Dog.find(params[:id])
+    @dog.update!(name: params[:name], breed: params[:breed], age: params[:age])
+
+    redirect_to :dogs
+  end
+
+  def destroy
+    @dog = Dog.find(params[:id]).delete
+
+    redirect_to :dogs
+  end
+end
